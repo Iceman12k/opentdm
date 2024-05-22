31958ce65759db1fd363670cbf05343aa5adbeb8
Reki <spiper212@gmail.com>
2024-05-21 20:08:52 -0400

    Machine Gun knockback 2 -> 28

--- p_weapon.c
+++ p_weapon.c
@@ -951,7 +951,7 @@ void Machinegun_Fire(edict_t *ent) {
     vec3_t forward, right;
     vec3_t angles;
     int damage = 8;
-    int kick = 2;
+    int kick = 28;
     vec3_t offset;
 
     if (!(ent->client->buttons & BUTTON_ATTACK)) {
